dd197b3751a557eebff984011641d3e569e33867
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-07-04 17:07:58 +1000

    updating variables

--- variables.tf
+++ variables.tf
@@ -1,12 +1,28 @@
 # main creds for AWS connection 
 # Don't do this. These creds are invalid and wont work
 # I'm doing this to test if it will be picked up by the static analysis
-variable "aws_access_key_id" {
-  description = "AWS access key"
-  default = "AKIAAOEVE2QMK6A4JZZM"
+# variable "aws_access_key_id" {
+#   description = "AWS access key"
+#   default = "AKIAAOEVE2QMK6A4JZZM"
+# }
+# variable "aws_secret_access_key" {
+#   description = "AWS secret access key"
+#   default = "Wh5ZNFWhn52EShu+HA2Jm6shMmJm3nw5ZbglpHxw"
+# }
+
+variable "region" {
+  type = string
+  default = "ap-southeast-2"
 }
 
-variable "aws_secret_access_key" {
-  description = "AWS secret access key"
-  default = "Wh5ZNFWhn52EShu+HA2Jm6shMmJm3nw5ZbglpHxw"
+variable "env" {
+  type = string
+  default = "production"
+  description = "The name of the Environment"
+}
+
+variable "team" {
+  type = string
+  default = "team-coffee"
+  description = "Team that owns the environmnet"
 }
