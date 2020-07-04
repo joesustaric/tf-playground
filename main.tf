154f9cf6ca5c7788db9272cbeea78953975d314f
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-07-04 15:55:00 +1000

    Adding in some credentials that dont work to test just to test the chekov tool

--- main.tf
+++ main.tf
@@ -1,16 +1,18 @@
 # Setup our aws provider
 provider "aws" {
+  # Don't commit keys, I have for testing.. but they're invalid.
   # access_key = "${var.aws_access_key_id}"
   # secret_key = "${var.aws_secret_access_key}"
-  # region = "${var.vpc_region}"
+
   version = "~> 2.0"
   region = "ap-southeast-2"
 }
 
 # Define a vpc
-resource "aws_vpc" "joes-vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "joes-vpc" { # "aws_vps" = type , "joes-vpc" = name
+  cidr_block = "10.0.0.0/16" # 65536 ip addresses
   tags = {
     Name = "JoesTestVPC"
+    Team = "teamFoo"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,12 @@
-variable "availability_zone" {
-  description = "availability zone used for the demo, based on region"
-  default = {
-    us-east-1 = "us-east-1a"
-    us-west-1 = "us-west-1a"
-  }
+# main creds for AWS connection 
+# Don't do this. These creds are invalid and wont work
+# I'm doing this to test if it will be picked up by the static analysis
+variable "aws_access_key_id" {
+  description = "AWS access key"
+  default = "AKIAAOEVE2QMK6A4JZZM"
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS secret access key"
+  default = "Wh5ZNFWhn52EShu+HA2Jm6shMmJm3nw5ZbglpHxw"
 }
