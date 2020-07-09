5c484ca2627b703f23604fc0d12a73f35c1cfd29
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-07-09 12:25:59 +1000

    another subnet

--- modules/vpc/public_subnets.tf
+++ modules/vpc/public_subnets.tf
@@ -1,4 +1,4 @@
-# public subnet a
+# Public Subnet a
 
 # Public subnet is a subset of IP's which can access the internet via
 # the internet gateway
@@ -15,5 +15,25 @@ resource "aws_subnet" "public_subnet_a" {
 
 resource "aws_route_table_association" "public_subnet_a" {
   subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.igw.id # route traffic through the Internet Gateway (public)
+}
+
+# Public Subnet b
+
+# Public subnet is a subset of IP's which can access the internet via
+# the internet gateway
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id               = aws_vpc.main.id
+  cidr_block           = var.public_subnet_b_cidr
+  availability_zone_id = var.az_zone_b_id
+
+  tags = {
+    Name = "public-subnet-b-${var.team}"
+    Env  = var.env
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_b" {
+  subnet_id      = aws_subnet.public_subnet_b.id
   route_table_id = aws_route_table.igw.id # route traffic through the Internet Gateway (public)
 }
