faeac34c51cdc1f21c0f25677fee4775ac134860
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-07-19 22:07:39 +1000

    more tersting

--- scripts/check.sh
+++ scripts/check.sh
@@ -7,4 +7,8 @@ checkov -d .
 
 echo "Checking if IAM Config is in sync....."
 echo "todo assume role"
-aws --help
+aws sts assume-role --role-arn "arn:aws:iam::580133377048:role/CI-Role" --role-session-name Travis-CI
+
+echo " Installing Iamy.."
+echo "${PATH}"
+curl https://github.com/99designs/iamy/releases/download/v2.3.2/iamy-darwin-amd64 --output iamy
