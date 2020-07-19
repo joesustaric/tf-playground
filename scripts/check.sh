ae063ecc76ae26ef9d970614c467219df5fd3a5c
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-07-19 22:42:59 +1000

    debuggin

--- scripts/check.sh
+++ scripts/check.sh
@@ -13,7 +13,7 @@ echo "todo assume role"
 aws sts assume-role --role-arn "arn:aws:iam::580133377048:role/CI-Role" --output json --role-session-name Travis-CI > /dev/null 2>&1
 
 echo " Installing Iamy.."
-curl https://github.com/99designs/iamy/releases/download/v2.3.2/iamy-darwin-amd64 --output iamy
+curl -L https://github.com/99designs/iamy/releases/download/v2.3.2/iamy-darwin-amd64 --output iamy
 chmod +x iamy
 ./iamy pull -d ./global/IAM
 
