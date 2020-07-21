38f2ba3078f4323493b49efc75a418113f0922e3
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-07-21 22:34:57 +1000

    script check

--- scripts/check.sh
+++ scripts/check.sh
@@ -6,12 +6,6 @@ echo "Running chekov....."
 checkov -d .
 
 echo "Checking if IAM Config is in sync....."
-echo "todo assume role"
-
-# The output of the assume role command will log out the AWS secrets.. 
-# TODO - Find a better way to do this with Travis CI..
-# aws sts assume-role --role-arn "arn:aws:iam::580133377048:role/CI-Role" --output json --role-session-name travis-ci > /dev/null 2>&1
-
 
 ## Fix this not working...
 echo " Installing Iamy.."
@@ -19,6 +13,7 @@ curl -L https://github.com/99designs/iamy/releases/download/v2.3.2/iamy-linux-am
 chmod +x iamy_ci
 AWS_REGION=ap-southeast-2 ./iamy_ci pull -d ./global/IAM
 
+# Check for any new or modified git files.
 if [[ $(git add -A -n | wc -l) -ge 0 ]]; then
     echo "IAM changes.. Needs attention"
     exit 1
