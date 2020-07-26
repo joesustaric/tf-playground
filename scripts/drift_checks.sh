75182fb8d7f6c4c72ae52656a2630c44b1ce864d
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-07-26 15:27:02 +1000

    stupid quotes

--- scripts/drift_checks.sh
+++ scripts/drift_checks.sh
@@ -4,10 +4,10 @@ set -xo pipefail
 
 export TF_VERSION=$(cat .version)
 
-wget https://releases.hashicorp.com/terraform/$(TF_VERSION)/terraform_$(TF_VERSION)_linux_amd64.zip
-unzip terraform_$(TF_VERSION)_linux_amd64.zip
+wget --quiet "https://releases.hashicorp.com/terraform/$TF_VERSION/terraform_"$TF_VERSION"_linux_amd64.zip"
+unzip "terraform_"$TF_VERSION"_linux_amd64.zip"
 mv terraform /usr/bin
-rm terraform_$(TF_VERSION)_linux_amd64.zip
+rm "terraform_"$TF_VERSION"_linux_amd64.zip"
 
 terraform
 
