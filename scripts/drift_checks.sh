38233b30a3c882d054618b9476e0843f3600e509
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-07-26 15:30:51 +1000

    add pwd to path

--- scripts/drift_checks.sh
+++ scripts/drift_checks.sh
@@ -3,10 +3,10 @@
 set -xo pipefail
 
 export TF_VERSION=$(cat .version)
+export PATH=$PATH:$(pwd)
 
 wget --quiet "https://releases.hashicorp.com/terraform/$TF_VERSION/terraform_"$TF_VERSION"_linux_amd64.zip"
 unzip "terraform_"$TF_VERSION"_linux_amd64.zip"
-mv terraform /usr/bin
 rm "terraform_"$TF_VERSION"_linux_amd64.zip"
 
 terraform
