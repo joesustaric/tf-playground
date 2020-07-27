3233f1f2dde9f65f69deb4cfee750ea52250bcb8
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-07-27 21:15:14 +1000

    adding in zone

--- scripts/drift_checks.sh
+++ scripts/drift_checks.sh
@@ -4,6 +4,7 @@ set -xo pipefail
 
 export TF_VERSION=$(cat .version)
 export PATH=$PATH:$(pwd)
+export AWS_REGION=ap-southeast-2
 
 wget --quiet "https://releases.hashicorp.com/terraform/$TF_VERSION/terraform_"$TF_VERSION"_linux_amd64.zip"
 unzip "terraform_"$TF_VERSION"_linux_amd64.zip"
