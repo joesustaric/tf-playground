68abe06b1a5fa5d7d575eafbe0b411b2730b937c
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-08-02 11:02:57 +1000

    define a foo-app test definition with an id to some secret

---
+++ prod/foo-app/main.tf
@@ -0,0 +1,4 @@
+# Set up backend to store .tfstate file
+# https://www.terraform.io/docs/backends/types/index.html
+# https://www.terraform.io/docs/backends/types/s3.html
+
---
+++ prod/foo-app/variables.tf
@@ -0,0 +1,4 @@
+variable "foo_token_id" {
+  type        = string
+  description = "Secret Token"
+}
