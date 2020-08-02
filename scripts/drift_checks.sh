de63b8b68140c9e551c8b9f9dff7e73b1f905271
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-08-02 11:29:35 +1000

    add in the CI drift check and a todo note

--- scripts/drift_checks.sh
+++ scripts/drift_checks.sh
@@ -10,6 +10,13 @@ wget --quiet "https://releases.hashicorp.com/terraform/$TF_VERSION/terraform_"$T
 unzip "terraform_"$TF_VERSION"_linux_amd64.zip"
 rm "terraform_"$TF_VERSION"_linux_amd64.zip"
 
+# TODO - Find a better synamic way to traverse the definitions and run the checks
+# rather than having to add them in here 1 by 1. 
+
 cd prod/vpc
+terraform init
+terraform plan -var-file=prod.tfvars -detailed-exitcode
+
+cd ../foo-app
 terraform init
 terraform plan -var-file=prod.tfvars -detailed-exitcode
