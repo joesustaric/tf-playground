daa44685c33406b98c897ab220d7ee4b52253815
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-08-02 11:03:28 +1000

    add in the encrypted backend reference

--- prod/foo-app/main.tf
+++ prod/foo-app/main.tf
@@ -2,3 +2,13 @@
 # https://www.terraform.io/docs/backends/types/index.html
 # https://www.terraform.io/docs/backends/types/s3.html
 
+terraform {
+  required_version = "~> 0.12"
+  backend "s3" {
+    encrypt = true
+    bucket  = "joes-tf-state-encrypted"
+    key     = "prod/foo-app/terraform.tfstate"
+    region  = "ap-southeast-2"
+  }
+}
+
