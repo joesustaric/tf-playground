9a0e80bedc729526a5fdfa5831601820f32c58f0
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-08-06 19:58:26 +1000

    removed ecs module.. not going to a module yet

--- modules/ecs/main.tf
+++
@@ -1,8 +0,0 @@
-## create roles via IAMY? - no probs not keep it here.. sync to IAMY maybe..
-
-# create cluster - ref VPC outputs for input state
-
-# use latest ecs image from aws
-# max 2 min 1 desired 1
-
-# deploy a thing to it with a ALB .. slack bot? react app?
