ea4d1b3a3cce3e24f37d18803963a0efd0239d2f
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-08-06 21:22:02 +1000

    adding in the vpc output and the subnest

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,4 +1,14 @@
-output "VPCId" {
+output "vpc_id" {
   description = "The ID of the VPC"
   value       = aws_vpc.main.id
+}
+
+output "private_subnet_a_id" {
+  description = "The ID of private subnet a"
+  value       = aws_subnet.private_subnet_a.id
+}
+
+output "private_subnet_b_id" {
+  description = "The ID of private subnet b"
+  value       = aws_subnet.private_subnet_b.id
 }
